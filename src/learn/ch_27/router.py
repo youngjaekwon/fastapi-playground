f3bcaead38aafa608cc23f9e4106cc9eff0d8b32
youngjaekwon <gczzga@gmail.com>
2025-02-06 13:52:57 +0900

    learn: docs ch.27

---
+++ src/learn/ch_27/router.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+
+router = APIRouter(prefix="/ch-27", tags=["ch.27"])
+
+
+class Item(BaseModel):
+    name: str | None = None
+    description: str | None = None
+    price: float | None = None
+    tax: float = 10.5
+    tags: list[str] = []
+
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+
+@router.get("/items/{item_id}", response_model=Item)
+async def read_item(item_id: str):
+    return items[item_id]
+
+
+@router.put("/items/{item_id}", response_model=Item)
+async def replace_item(item_id: str, item: Item):
+    update_item_encoded = jsonable_encoder(item)
+    items[item_id] = update_item_encoded
+    return update_item_encoded
+
+
+@router.patch("/items/{item_id}", response_model=Item)
+async def update_item(item_id: str, item: Item):
+    stored_item_data = items[item_id]
+    stored_item_model = Item(**stored_item_data)
+
+    update_data = item.model_dump(exclude_unset=True)
+    updated_item = stored_item_model.model_copy(update=update_data)
+    items[item_id] = jsonable_encoder(updated_item)
+    return updated_item
--- src/learn/main.py
+++ src/learn/main.py
@@ -16,6 +16,7 @@ from src.learn.ch_24.router import (
 )
 from src.learn.ch_25.router import router as ch_25_router
 from src.learn.ch_26.router import router as ch_26_router
+from src.learn.ch_27.router import router as ch_27_router
 
 app = FastAPI()
 app.include_router(ch_19_router)
@@ -29,3 +30,4 @@ app.exception_handler(StarletteHTTPException)(custom_http_exception_handler)
 app.exception_handler(RequestValidationError)(validation_exception_handler)
 app.include_router(ch_25_router)
 app.include_router(ch_26_router)
+app.include_router(ch_27_router)
