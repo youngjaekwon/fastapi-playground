0b666a06c27556308ca678d2eca1c56acad94535
youngjaekwon <gczzga@gmail.com>
2025-02-13 13:56:52 +0900

    learn: docs ch.35

---
+++ src/learn/ch_35/router.py
@@ -0,0 +1,14 @@
+import time
+
+from fastapi import APIRouter, Request
+
+router = APIRouter(prefix="/ch-35", tags=["ch.35"])
+
+
+# app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
