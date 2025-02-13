02954528aea0b2e1f16cef453db071c11f838102
youngjaekwon <gczzga@gmail.com>
2025-02-13 16:51:03 +0900

    learn: docs ch.40

--- src/learn/main.py
+++ src/learn/main.py
@@ -28,7 +28,57 @@ from src.learn.ch_37.router import router as ch_37_router, on_startup
 from src.learn.ch_38.main import router as ch_38_router
 from src.learn.ch_39.router import router as ch_39_router
 
-app = FastAPI()
+description = """
+ChimichangApp API helps you do awesome stuff.
+
+## Items
+
+You can **read items**.
+
+## Users
+
+You will be able to:
+
+* **Create users** (_not implemented_).
+* **Read users** (_not implemented_).
+"""
+
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "items",
+        "description": "Manage items. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+]
+
+app = FastAPI(
+    title="ChimichangApp",
+    description=description,
+    summary="Deadpool's favorite app. Nuff said.",
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Deadpoolio the Amazing",
+        "url": "http://x-force.example.com/contact/",
+        "email": "dp@x-force.example.com",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        # "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+        "identifier": "MIT",
+    },
+    openapi_tags=tags_metadata,
+    openapi_url="/api/v1/openapi.json",
+    # docs_url="/documentation",
+    redoc_url=None,
+)
 app.include_router(ch_19_router)
 app.include_router(ch_20_router)
 app.include_router(ch_21_router)
